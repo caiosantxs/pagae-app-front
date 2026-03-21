2f007e85b0a0e5d281f274330fd1505f2d18095e
Caio Guilherme <caioonolide@hotmail.com>
2026-03-21 02:00:22 -0400

    fix: corrigindo URL no env de produção

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://pagae.page/api',
+  apiUrl: 'http://localhost:8080/api',
   googleClientId: '800410267605-uouikhhivske3srviojnmfr2ad3mgc0p.apps.googleusercontent.com'
 };
