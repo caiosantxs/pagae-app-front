e8e6db2822afec35a9bc570154fac093a49fa515
Caio Guilherme <caioonolide@hotmail.com>
2026-04-07 02:07:54 -0400

    fix: ajustando apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://pagae.page/api',
   googleClientId: '800410267605-uouikhhivske3srviojnmfr2ad3mgc0p.apps.googleusercontent.com'
 };
