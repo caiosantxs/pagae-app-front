b742b267fd89505931dbf0791684dd1212654c23
Caio Guilherme <caioonolide@hotmail.com>
2026-04-08 22:07:17 -0400

    Reapply "Merge pull request #2 from caiosantxs/developer"
    This reverts commit 6324113562fc2b2abfb15e7e6389ff1baef911e6.

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { LoginService } from '../../pages/login/login-service'; // Ajuste o caminho
+import { LoginService } from '../../pages/auth/login/login-service'; // Ajuste o caminho
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const loginService = inject(LoginService);
